14495dfeed3d082d5867d37f284777222d1984d8
Theophilus Confidence <spanzytheo@gmail.com>
2025-10-10 18:07:00 +0100

    adjustments in some pages

--- app/(main)/components/blog/BlogHero.tsx
+++ app/(main)/components/blog/BlogHero.tsx
@@ -26,7 +26,7 @@ const BlogHero = () => {
       className="relative min-h-[500px] md:min-h-[600px] lg:min-h-[550px]"
     >
       <div className="absolute inset-0 bg-gradient-to-b from-[#111111]/60 via-[#111111]/70 to-[#111111]/80 z-0" />
-      <div className="relative z-10 flex flex-col items-center justify-center h-full min-h-[500px] md:min-h-[600px] lg:min-h-[550px] text-white gap-8 md:gap-4 px-5">
+      <div className="relative z-10 flex flex-col items-center justify-center h-full min-h-[500px] md:min-h-[600px] lg:min-h-[550px] text-white gap-3 md:gap-4 px-5">
         <h1
           className={`${playfair.className} text-[31px] md:text-[55px] lg:text-[65px] leading-[38px] md:leading-[60px] lg:leading-[71px] text-center max-w-[90%] md:max-w-[80%]`}
         >
