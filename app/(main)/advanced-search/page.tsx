17d460e51db06de9b7ec544c96cc69e97ed8243d
Theophilus Confidence <spanzytheo@gmail.com>
2026-01-13 20:19:40 +0100

    use dynamic import in advanced-search

--- app/(main)/advanced-search/page.tsx
+++ app/(main)/advanced-search/page.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useState, useCallback } from "react";
-import Map from "../components/advancedSearch/Map";
+// import Map from "../components/advancedSearch/Map";
 import MapResults from "../components/advancedSearch/MapResults";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../components/advancedSearch/Map"), {
+  ssr: false,
+});
 
 const AdvancedSearch = () => {
   const [filteredProperties, setFilteredProperties] = useState<any[]>([]);
@@ -10,7 +15,7 @@ const AdvancedSearch = () => {
   const handleFilteredResults = useCallback((results: any[]) => {
     setFilteredProperties(results);
   }, []);
-  
+
   return (
     <div className="mt-[4.1875rem] lg:mt-[6rem] flex flex-col md:flex-row md:justify-between">
       <Map properties={filteredProperties} />
